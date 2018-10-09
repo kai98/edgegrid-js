f26c34516846eec1070dcc8f1605e95b7ea86add
kaipus <kaipus@gmail.com>
2018-10-08 23:53:30 -0700

    git page

---
+++ client/build/precache-manifest.f626819c1839bcc904bf02b18818884d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b66e945f368b6067a21",
+    "url": "/edgegrid-js/client/static/js/runtime~main.5b66e945.js"
+  },
+  {
+    "revision": "2a3006c9d566a53575ec",
+    "url": "/edgegrid-js/client/static/js/main.2a3006c9.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/edgegrid-js/client/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "2a3006c9d566a53575ec",
+    "url": "/edgegrid-js/client/static/css/main.9ec5274b.chunk.css"
+  },
+  {
+    "revision": "6fbb6735fb796858b6ee25c1014cca87",
+    "url": "/edgegrid-js/client/index.html"
+  }
+];
